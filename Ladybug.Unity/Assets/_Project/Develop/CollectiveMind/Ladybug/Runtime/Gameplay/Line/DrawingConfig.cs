bad0e83949121b9d6ae0c5e21f1c21ad9031a381
Subbota Mikhail <mihail.subbota@inbox.ru>
2025-02-04 19:24:25 +0300

    feat: Add line drawing

---
+++ Ladybug.Unity/Assets/_Project/Develop/CollectiveMind/Ladybug/Runtime/Gameplay/Line/DrawingConfig.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace CollectiveMind.Ladybug.Runtime.Gameplay.Line
+{
+  [CreateAssetMenu(menuName = CAC.Names.DRAWING_CONFIG_MENU, fileName = CAC.Names.DRAWING_CONFIG_FILE)]
+  public class DrawingConfig : ScriptableObject
+  {
+    public ComputeShader BrushDrawerShader;
+    public LayerMask CanvasLayer;
+    public float BrushRadius;
+    public Color BrushColor;
+    public int TextureSize;
+  }
+}
