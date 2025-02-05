98966c34ece02ea4aab618c04fcdb2003c22c8b8
Subbota Mikhail <mihail.subbota@inbox.ru>
2025-02-05 15:58:24 +0300

    feat: Draw on canvas with flying cursor

--- Ladybug.Unity/Assets/_Project/Develop/CollectiveMind/Ladybug/Runtime/Gameplay/Line/LineDrawer.cs
+++ Ladybug.Unity/Assets/_Project/Develop/CollectiveMind/Ladybug/Runtime/Gameplay/Line/LineDrawer.cs
@@ -17,7 +17,9 @@ namespace CollectiveMind.Ladybug.Runtime.Gameplay.Line
     private readonly RenderTexture _renderTexture;
     private readonly int _kernelIndex;
 
-    private Vector2 _lastPosition;
+    private Vector2 _lastPoint;
+    private Renderer _lastCanvas;
+    private bool _isDrawing;
 
     public LineDrawer(IConfigProvider configProvider)
     {
@@ -37,37 +39,81 @@ namespace CollectiveMind.Ladybug.Runtime.Gameplay.Line
 
     public void Tick()
     {
-      if (Input.GetMouseButton(0) && Physics.Raycast(_mainCamera.ScreenPointToRay(Input.mousePosition),
-        out RaycastHit hit, Mathf.Infinity, _config.CanvasLayer))
+      if (Input.GetMouseButton(0))
       {
-        Vector2 screenPoint = hit.textureCoord;
-        if (Input.GetMouseButtonDown(0))
-          _lastPosition = screenPoint;
-
-        var renderer = hit.collider.GetComponent<Renderer>();
-        Texture2D texture = (Texture2D)renderer.material.mainTexture;
-        if (texture == null)
+        Vector2 currentPoint = Vector2.zero;
+        if (Physics.Raycast(_mainCamera.ScreenPointToRay(Input.mousePosition),
+          out RaycastHit hit, Mathf.Infinity, _config.CanvasLayer))
         {
-          var resultTexture = new Texture2D(_config.TextureSize, _config.TextureSize, TextureFormat.RGBA32, false);
+          currentPoint = hit.textureCoord;
+          if (!_isDrawing)
+          {
+            _lastPoint = currentPoint;
+            _isDrawing = true;
+          }
 
-          texture = resultTexture;
-          renderer.material.mainTexture = resultTexture;
+          _lastCanvas = hit.collider.GetComponent<Renderer>();
+        }
+        else if (_lastCanvas)
+        {
+          currentPoint = GetUVCursorPoint();
         }
 
-        _config.BrushDrawerShader.SetTexture(_kernelIndex, _result, _renderTexture);
-        _config.BrushDrawerShader.SetVector(_segmentStart, _lastPosition);
-        _config.BrushDrawerShader.SetVector(_segmentEnd, screenPoint);
-        _config.BrushDrawerShader.SetFloat(_brushRadius, _config.BrushRadius / _config.TextureSize);
-        _config.BrushDrawerShader.SetVector(_brushColor, _config.BrushColor);
+        if (_lastCanvas)
+        {
+          Texture2D texture = GetTexture();
+          DrawLine(texture, currentPoint);
+        }
 
-        _config.BrushDrawerShader.Dispatch(_kernelIndex, _config.TextureSize / 8, _config.TextureSize / 8, 1);
-
-        RenderTexture.active = _renderTexture;
-        texture.ReadPixels(new Rect(0, 0, _config.TextureSize, _config.TextureSize), 0, 0);
-        texture.Apply();
-
-        _lastPosition = screenPoint;
+        _lastPoint = currentPoint;
       }
+
+      if (Input.GetMouseButtonUp(0))
+      {
+        _lastCanvas = null;
+        _isDrawing = false;
+      }
+    }
+
+    private Vector2 GetUVCursorPoint()
+    {
+      var deepMousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y,
+        Mathf.Abs(_mainCamera.transform.position.y - _lastCanvas.transform.position.y));
+      Vector3 worldMousePosition = _mainCamera.ScreenToWorldPoint(deepMousePosition);
+      Vector3 localMousePosition = _lastCanvas.transform.InverseTransformPoint(worldMousePosition);
+      Vector2 localTexturePoint = new Vector2(localMousePosition.x, localMousePosition.z);
+
+      Vector2 canvasSize = 10 * _lastCanvas.transform.localScale;
+      return -(localTexturePoint - canvasSize / 2) / canvasSize;
+    }
+
+    private Texture2D GetTexture()
+    {
+      Texture2D texture = (Texture2D)_lastCanvas.material.mainTexture;
+      if (texture == null)
+      {
+        var resultTexture = new Texture2D(_config.TextureSize, _config.TextureSize, TextureFormat.RGBA32, false);
+
+        texture = resultTexture;
+        _lastCanvas.material.mainTexture = resultTexture;
+      }
+
+      return texture;
+    }
+
+    private void DrawLine(Texture2D texture, Vector2 texturePoint)
+    {
+      _config.BrushDrawerShader.SetTexture(_kernelIndex, _result, _renderTexture);
+      _config.BrushDrawerShader.SetVector(_segmentStart, _lastPoint);
+      _config.BrushDrawerShader.SetVector(_segmentEnd, texturePoint);
+      _config.BrushDrawerShader.SetFloat(_brushRadius, _config.BrushRadius / _config.TextureSize);
+      _config.BrushDrawerShader.SetVector(_brushColor, _config.BrushColor);
+
+      _config.BrushDrawerShader.Dispatch(_kernelIndex, _config.TextureSize / 8, _config.TextureSize / 8, 1);
+
+      RenderTexture.active = _renderTexture;
+      texture.ReadPixels(new Rect(0, 0, _config.TextureSize, _config.TextureSize), 0, 0);
+      texture.Apply();
     }
   }
 }
