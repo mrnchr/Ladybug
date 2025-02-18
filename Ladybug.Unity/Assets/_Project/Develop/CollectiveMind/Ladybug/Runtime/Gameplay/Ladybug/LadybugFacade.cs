7bbd32ce827cb1ab20abb2dde0e116201b0e1943
Subbota Mikhail <mihail.subbota@inbox.ru>
2025-02-18 18:15:30 +0300

    feat: Import and set ladybug and obstacles

--- Ladybug.Unity/Assets/_Project/Develop/CollectiveMind/Ladybug/Runtime/Gameplay/Ladybug/LadybugFacade.cs
+++ Ladybug.Unity/Assets/_Project/Develop/CollectiveMind/Ladybug/Runtime/Gameplay/Ladybug/LadybugFacade.cs
@@ -1,5 +1,6 @@
 ﻿using CollectiveMind.Ladybug.Runtime.Configuration;
 using CollectiveMind.Ladybug.Runtime.Infrastructure.Visual;
+using R3;
 using UnityEngine;
 
 namespace CollectiveMind.Ladybug.Runtime.Gameplay.Ladybug
@@ -13,11 +14,14 @@ namespace CollectiveMind.Ladybug.Runtime.Gameplay.Ladybug
     
     public Transform Transform => _visual.transform;
     public Vector3 Velocity { get; private set; }
+    public ReactiveProperty<bool> IsMoving { get; }
 
     public LadybugFacade(IConfigProvider configProvider, ILadybugRotator rotator)
     {
       _rotator = rotator;
       _config = configProvider.Get<LadybugConfig>();
+      
+      IsMoving = new ReactiveProperty<bool>(false);
     }
 
     public void SetVisual(LadybugVisual visual)
@@ -28,6 +32,7 @@ namespace CollectiveMind.Ladybug.Runtime.Gameplay.Ladybug
     public void UpdateVelocity(Vector3 direction)
     {
       Velocity = direction * _config.Speed;
+      IsMoving.Value = Velocity != Vector3.zero;
     }
 
     public void CheckBound()
