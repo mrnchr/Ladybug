93cf0d0d3849c177975c5be3301c66391b76983b
Subbota Mikhail <mihail.subbota@inbox.ru>
2025-09-26 23:33:38 +0300

    build: Use PluginYG2 template for build

--- yarun.cmd
+++ yarun.cmd
@@ -4,3 +4,5 @@ set buildFolder=%~2
 if "%buildFolder%"=="" set buildFolder=Ladybug
 
 start npx @yandex-games/sdk-dev-proxy -p ".artifacts/Web/%buildFolder%" -i="%appId%" -с
+start cloudpub
+start https://cloudpub.cloudpub.ru
