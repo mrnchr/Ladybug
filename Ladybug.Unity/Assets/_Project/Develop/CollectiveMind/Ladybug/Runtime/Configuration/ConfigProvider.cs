80c3a2c26dbaddaf07fd725210eee7c48c80b02a
Subbota Mikhail <mihail.subbota@inbox.ru>
2026-04-14 20:19:08 +0300

    feat: Create config provider asset

---
+++ Ladybug.Unity/Assets/_Project/Develop/CollectiveMind/Ladybug/Runtime/Configuration/ConfigProvider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CollectiveMind.Ladybug.Runtime.Configuration
+{
+  [CreateAssetMenu(menuName = CAC.CONFIG_MENU + nameof(ConfigProvider), fileName = nameof(ConfigProvider))]
+  public class ConfigProvider : ScriptableObject
+  {
+    public List<ScriptableObject> Configs;
+  }
+}
